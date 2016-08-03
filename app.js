020df98e95a6a03efd44395ea792966767d9642d
chengyurou <18829292695@163.com>
2016-08-03 14:13:00 -0400

    finish 6 time use 21 minites

--- app.js
+++ app.js
@@ -1,70 +1,69 @@
 const App = React.createClass({
-
     getInitialState:function () {
-        return {
+        return{
             isEditor:true,
             elements:[]
         }
     },
-    toogle:function () {
-        this.setState({isEditor:!this.state.isEditor});
+    toogle:function(){
+        this.setState({isEditor:!this.state.isEditor})
     },
     add:function (element) {
-        const elements = this.state.elements;
+       const elements = this.state.elements;
+        console.log(element);
         elements.push(element)
 
-        this.setState({elements});
+        this.setState({elements})
     },
-    remove:function (i) {
+    onRemove:function (i) {
         const elements = this.state.elements;
-        elements.splice(i,1);
+        elements.splice(i,1)
 
-        this.setState({elements});
+        this.setState({elements})
     },
     render: function(){
         var isEditor = this.state.isEditor;
         return (
             <div>
-                <button onClick={this.toogle}>{isEditor?'preview' : 'edit'}</button>
+                <button onClick={this.toogle}>{isEditor?'preview' :'edie'}</button>
                 <div className={isEditor?'':'hidden'}>
-                    <Editor onAdd={this.add} elements={this.state.elements} onRemove={this.remove}/>
+                    <Editor onAdd={this.add} onRemove={this.onRemove} elements={this.state.elements}/>
                 </div>
                 <div className={isEditor?'hidden':''}>
                     <Preview elements={this.state.elements}/>
                 </div>
             </div>
         )
-    }   
+    }
 });
 
 const Editor = React.createClass({
-   
+
     render: function(){
         return (
             <div>
                 <div>
-                    <Left elements={this.props.elements} onRemove={this.props.onRemove}/>
+                    <Left onRemove={this.props.onRemove} elements={this.props.elements}/>
                 </div>
-                <div>
-                    <Right onAdd={this.props.onAdd}/>
-                </div>
+                <div><Right onAdd={this.props.onAdd}/></div>
             </div>
         )
-    }   
+    }
 });
 
 const Left = React.createClass({
-
     remove:function (i) {
-        console.log(i);
         this.props.onRemove(i)
     },
+
     render: function(){
+
         const elements = this.props.elements.map((ele,i) => {
             return <div key={i}>
-                <input type={ele}/>
-                <button onClick={this.remove.bind(this,i)}>x</button>
-            </div>
+            <input type={ele}/>
+                <button onClick={this.remove}>x</button>
+                </div>
+
         })
         return (
             <div>{elements}</div>
@@ -87,18 +86,21 @@ const Right = React.createClass({
         )
     }
 });
+
 const Preview = React.createClass({
-   
+
     render: function(){
         const elements = this.props.elements.map((ele,i) => {
             return <div key={i}>
                 <input type={ele}/>
+
             </div>
+
         })
         return (
             <div>{elements}</div>
         )
-    }   
+    }
 });
 
 ReactDOM.render(<App/>,document.getElementById('content'))
